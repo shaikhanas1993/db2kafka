d4d8394472c55b706dbae3157f703617486e46ba
Denis Oblogin <ukrbublik@gmail.com>
2019-08-20 18:24:48 +0300

    support LES

--- web2kafka.sh
+++ web2kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DB_TABLE=pipe_to_localpipe TOPICS=escalation_policy_updates,user_updates,permissions,team_hierarchy BARRIER_PATH=/db2kafka_failover_barrier_localpipe iex -S mix &
+DB_TABLE=pipe_to_localpipe TOPICS=escalation_policy_updates,user_updates,permissions,team_hierarchy,incidents_by_account,incident_notification_updates BARRIER_PATH=/db2kafka_failover_barrier_localpipe iex -S mix &
 localpipe=$!
-DB_TABLE=pipe_to_bitpipe TOPICS=team_updates,entitlements BARRIER_PATH=/db2kafka_failover_barrier_bitpipe iex -S mix &
+DB_TABLE=pipe_to_bitpipe TOPICS=team_updates,entitlements,ile_group_requests,incidents_by_account,ihm_messages BARRIER_PATH=/db2kafka_failover_barrier_bitpipe iex -S mix &
 bitpipe=$!
 wait $localpipe $bitpipe
